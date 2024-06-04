53e0aa9ea2d979bafce11d463a89ff209d37b9f2
KasunMillawa <kasunmillawabandara@gmail.com>
2024-06-04 19:29:15 +0530

    create sign in api route and update env

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req , res , next) => {
 
@@ -23,3 +24,21 @@ const newUser = new User ({username,email,password: hashedPassword});
  
 
 }; 
+
+export const signin = async (req , res , next ) => {
+      const {email , password} = req.body;
+
+      try {
+            const validUser = await User.findOne ({ email});
+            if (!validUser) return next(errHandler(404 , 'user not found'));
+            const validPassword = bcryptjs.compareSync(password , validUser.password);
+            if (!validPassword) return next (errHandler(404 , 'wrong credentials'));
+            const token = jwt.sign({ id: validUser._id} , process.env.JWT_SECRET);
+            const {password: pass , ...rest} = validUser._doc;
+            res.cookie('access_token' , token , {httpOnly: true}).status(200).json(rest);
+            
+            
+      } catch (error) {
+            next(error);
+      }
+}
