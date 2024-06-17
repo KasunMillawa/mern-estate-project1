4ca2801e245c6ac42a0059c38dbfcddef79acd9f
KasunMillawa <kasunmillawabandara@gmail.com>
2024-06-17 07:55:27 +0530

    make profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,26 +3,23 @@ import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Signin from "./pages/Signin";
-//import Profile from "./Profile";
 import Header from "./components/Header";
 import Profile from "./pages/Profile";
+import PrivateRoute from "./components/privateRoute";
 
 export default function App() {
-  return <BrowserRouter>
-
-  <Header/>
-
-  <Routes>
-
-  <Route path='/' element={<Home/>} />
-  <Route path='/sign-up' element={<SignUp/>} />
-  <Route path='/about' element={<About/>} />
-  <Route path='/sign-in' element={<Signin/>} />
-  <Route path='/profile' element={<Profile/>} />
-
-
-
-  </Routes>
-  </BrowserRouter>
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/sign-up' element={<SignUp />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/sign-in' element={<Signin />} />
+                <Route element={<PrivateRoute />}>
+                    <Route path='/profile' element={<Profile />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
-
