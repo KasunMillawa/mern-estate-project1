f5dd555ac37e6e4c9f2d44090f2d5fa3225dec27
KasunMillawa <kasunmillawabandara@gmail.com>
2024-07-06 21:34:03 +0530

    create delete user function

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -37,3 +37,18 @@ export const updateUser = async(req , res , next) => {
     }    
 
 };
+
+export const deleteUser = async (req , res , next) => {
+if (req.user.id !== req.params.id)
+    return next (errHandler(401, 'you can update only your account '));
+
+try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie('access_token');
+    res.status(200).json('user has been deleted ');
+} catch (error) {
+    next(error);
+}
+
+     
+};
