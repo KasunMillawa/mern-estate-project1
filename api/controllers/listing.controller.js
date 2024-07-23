9103514601043e54d7ebb00ef719abfa91bae5cd
KasunMillawa <kasunmillawabandara@gmail.com>
2024-07-23 18:48:20 +0530

    create delete user listings api

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errHandler } from "../utils/error.js";
 
 export const createListing = async ( req , res , next) => {
 
@@ -11,3 +12,23 @@ export const createListing = async ( req , res , next) => {
      
 
 }; 
+
+export const deleteListing = async (req , res , next) => {
+    const listing = await Listing.findById(req.params.id);
+
+    if(!listing) {
+        return next(errHandler(404, 'Listing not fount'));
+    }
+
+    if(req.user.id !== listing.userRef) {
+        return next(errHandler(401, 'you can only update your own listing..'));
+    }
+
+    try {
+        await Listing.findByIdAndDelete(req.params.id);
+        res.status(200).json('Your listing has been deleted');
+    } catch (error) {
+        next(error);
+    }
+
+};
