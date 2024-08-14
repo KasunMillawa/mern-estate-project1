39cfa04ca38c7e8719c5fd6453b1eecaa6c9cc89
KasunMillawa <kasunmillawabandara@gmail.com>
2024-08-14 11:32:17 +0530

    create search page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import PrivateRoute from "./components/privateRoute";
 import CreateListing from "./page1/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
+import Search from "./pages/Search";
 
 export default function App() {
     return (
@@ -19,6 +20,7 @@ export default function App() {
                 <Route path='/sign-up' element={<SignUp />} />
                 <Route path='/about' element={<About />} />
                 <Route path='/sign-in' element={<Signin />} />
+                <Route path='/search' element={<Search />} />
                 <Route path='/listing/:listingId' element={<Listing />} />
 
                 <Route element={<PrivateRoute />}>
